9f5cf8845ada8b90600567de0b29ecef605d724c
Guillermo Lara <grlara9@gmail.com>
2019-12-19 18:35:35 -0800

    work on the css on each component to align images and header

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./container.css";
 
-const Container = props => <div className="container">{props.children}</div>;
+const Container = props => 
+<div className="container">{props.children}
+</div>;
 
 export default Container;
