995eedbc7cf204ad597f1602300a052af8537edf
Guillermo Lara <grlara9@gmail.com>
2021-02-10 22:13:12 -0800

    modified all images and modified images css

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -3,22 +3,12 @@ import "./game.css";
 
 function Game(props){
     
-    let backgroundStyles = {
-        backgroundImage:`url(${props.image}`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "auto",
-        height: "17rem",
-        width: "15rem",
-        cursor: "pointer"
-    }
-
     return (
         <div className="col-md-4 mb-3">
             <div className="game" onClick={() => props.clickCount(props.id)}>
                 <div className="img-game">
-                    <div className="game-image" style={backgroundStyles}></div>
-                    {/* <img alt={props.name} src={props.image} /> */}
+                    <div className="game-image"></div>
+                    { <img alt={props.name} src={props.image} /> }
                 </div>
             </div>
         </div>
